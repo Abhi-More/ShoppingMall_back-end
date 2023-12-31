2326fdc69a147d746015da45ed35311aaa401aea
Abhishek More <amore8196@gmail.com>
2023-12-31 19:47:58 +0530

    Committing starter project

---
+++ MallManager/src/main/java/com/shoppingmall/demo/MallManagerApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MallManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MallManagerApplication.class, args);
+	}
+
+}
---
+++ MallManager/src/test/java/com/shoppingmall/demo/MallManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoppingmall.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
