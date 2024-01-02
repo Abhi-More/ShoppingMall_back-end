9e524b12741275390961613228f5efbab501814c
sandip zalte <sandipzalte2002@gmail.com>
2024-01-02 20:39:07 +0530

    first commit/ parent root implemented(/user)

--- MallManager/src/main/java/com/shoppingmall/demo/controllers/UserController.java
+++ MallManager/src/main/java/com/shoppingmall/demo/controllers/UserController.java
@@ -3,26 +3,25 @@ package com.shoppingmall.demo.controllers;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.shoppingmall.demo.models.User;
 import com.shoppingmall.demo.services.UserInfoService;
 
+
 @RestController
+@RequestMapping("/user")
 public class UserController {
 	@Autowired
 	private UserInfoService service;
 	
-	@PostMapping("signup")
-	public String addUser(User user)
+	@PostMapping("/signup")
+	public String addUser(@RequestBody User user)
 	{
 		return service.addUser(user);
 	}
 	
-	@GetMapping("userProfile/{id}")
+	@GetMapping("/userProfile/{id}")
 	public Optional<User> getUser(@PathVariable("id") int id)
 	{
 		return service.getUserById(id);
