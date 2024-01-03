a12716e7dab3a89b2c96edb1b6b7e5eebd074a1f
sandip zalte <sandipzalte2002@gmail.com>
2024-01-03 10:40:53 +0530

    lombok dependency added (implicit getter/setter)

--- MallManager/src/main/java/com/shoppingmall/demo/models/User.java
+++ MallManager/src/main/java/com/shoppingmall/demo/models/User.java
@@ -4,8 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Data;
 
 @Entity
+@Data
 public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -15,38 +17,5 @@ public class User {
 	private String type;
 	private String email;
 	private String password;
-	
-	public User() {
-		super();
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	public String getType() {
-		return type;
-	}
-	public void setType(String type) {
-		this.type = type;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+
 }
