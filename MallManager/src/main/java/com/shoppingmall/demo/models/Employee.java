5798779b9ad606ab86c5a5d1f51956de10eb5c26
sandip zalte <sandipzalte2002@gmail.com>
2024-01-16 22:30:06 +0530

    Employee entity created with contact no. as unique id

--- MallManager/src/main/java/com/shoppingmall/demo/models/Employee.java
+++ MallManager/src/main/java/com/shoppingmall/demo/models/Employee.java
@@ -6,65 +6,71 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinTable;
-import jakarta.persistence.ManyToOne;
 
 @Entity
 public class Employee {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int empId;
-	
+//	@Id
+//	@GeneratedValue(strategy = GenerationType.IDENTITY)
+//	private int empId;
 	private String name;
-	private LocalDate dob;
-	private LocalDate dateOfJoining;
-	private float slary;
+	@Id()
+	private String contactNo;
+	private String dateOfJoining;
+	private float salary;
 	private String address;
 	private String designation;
-	
-	public Employee() {
-		super();
-	}
-	public int getEmpId() {
-		return empId;
-	}
-	public void setEmpId(int empId) {
-		this.empId = empId;
-	}
+
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	public LocalDate getDob() {
-		return dob;
+
+	public String getContactNo() {
+		return contactNo;
 	}
-	public void setDob(LocalDate dob) {
-		this.dob = dob;
+
+	public void setContactNo(String contactNo) {
+		this.contactNo = contactNo;
 	}
-	public float getSlary() {
-		return slary;
+
+	public String getDateOfJoining() {
+		return dateOfJoining;
 	}
-	public void setSlary(float slary) {
-		this.slary = slary;
+
+	public void setDateOfJoining(String dateOfJoining) {
+		this.dateOfJoining = dateOfJoining;
 	}
+
+	public float getSalary() {
+		return salary;
+	}
+
+	public void setSalary(float salary) {
+		this.salary = salary;
+	}
+
 	public String getAddress() {
 		return address;
 	}
+
 	public void setAddress(String address) {
 		this.address = address;
 	}
+
 	public String getDesignation() {
 		return designation;
 	}
+
 	public void setDesignation(String designation) {
 		this.designation = designation;
 	}
-	public LocalDate getDateOfJoining() {
-		return dateOfJoining;
+
+	public Employee() {
+		super();
 	}
-	public void setDateOfJoining(LocalDate dateOfJoining) {
-		this.dateOfJoining = dateOfJoining;
-	}
+
 }
