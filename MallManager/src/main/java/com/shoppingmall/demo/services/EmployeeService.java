45b325a5e83d9f04c936814bbdd092a20702c1c5
sandip zalte <sandipzalte2002@gmail.com>
2024-01-16 22:38:56 +0530

    All services, add employee with validation

--- MallManager/src/main/java/com/shoppingmall/demo/services/EmployeeService.java
+++ MallManager/src/main/java/com/shoppingmall/demo/services/EmployeeService.java
@@ -3,6 +3,7 @@ package com.shoppingmall.demo.services;
 import java.util.List;
 import java.util.Optional;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,47 +16,62 @@ import com.shoppingmall.demo.repositories.EmployeeRepo;
 public class EmployeeService {
 	@Autowired
 	private EmployeeRepo repo;
-	
-	public String addEmployee(Employee employee)
-	{
+
+	public ResponseEntity<String> addEmployee(Employee employee) {
+
+		if (repo.findBycontactNo(employee.getContactNo()).isPresent()) {
+			return new ResponseEntity<>("ContactNo already exists", HttpStatus.CONFLICT);
+		}
+
+		// If contactNo is unique, save the employee
 		repo.save(employee);
-		return "Employee Added Successfully";
+		return new ResponseEntity<>("Employee added successfully", HttpStatus.CREATED);
 	}
-	
+
 	public List<Employee> getAllEmployee()
 	{
 		return repo.findAll();
 	}
-	
-	public ResponseEntity<Employee> getEmployeeById(int id)
+
+	public ResponseEntity<Employee> getEmployeeById(String id)
 	{
-		Optional<Employee> existingEmployee = repo.findById(id);
-		
+		Optional<Employee> existingEmployee = repo.findBycontactNo(id);
+
 		if(existingEmployee.isPresent()){
 			return new ResponseEntity<>(existingEmployee.get(), HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-	public ResponseEntity<Employee> deleteEmployee(int id)
+	@Transactional
+	public ResponseEntity<Employee> deleteEmployee(String id)
 	{
-		Optional<Employee> existingEmployee = repo.findById(id);
+		Optional<Employee> existingEmployee = repo.findBycontactNo(id);
 
 		if(existingEmployee.isPresent()){
-			repo.deleteById(id);;
+			repo.deleteBycontactNo(id);
 			return new ResponseEntity<>(HttpStatus.OK);
 		}
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-	
-	public ResponseEntity<Employee> updateEmployee(int id, Employee emp)
-	{
-		Optional<Employee> existingEmployee = repo.findById(id);
 
-		if(existingEmployee.isPresent()){
-			repo.save(emp);
-			return new ResponseEntity<>(emp, HttpStatus.OK);
+	public ResponseEntity<Employee> updateEmployee(String id, Employee emp) {
+		Optional<Employee> existingEmployee = repo.findBycontactNo(id);
+
+		if (existingEmployee.isPresent()) {
+			Employee newEmp = existingEmployee.get();
+			newEmp.setName(emp.getName());
+			newEmp.setContactNo(emp.getContactNo());
+			newEmp.setDateOfJoining(emp.getDateOfJoining());
+			newEmp.setSalary(emp.getSalary());
+			newEmp.setAddress(emp.getAddress());
+			newEmp.setDesignation(emp.getDesignation());
+			repo.save(newEmp);
+
+			return new ResponseEntity<>(newEmp, HttpStatus.OK);
 		}
+
 		return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 	}
-	
+
+
 }
